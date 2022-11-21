240c750fc7e54082ff828783e2ccb2f2acd19ab2
wangrongding <wangrongding@qq.com>
2022-11-21 13:33:19 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -857,7 +857,7 @@ module.exports = (function(e, t) {
         console.error(`Unable to get gist\n${e}`);
       }
       const r = [];
-      for (let t = 0; t < Math.min(e.data.languages.length, 5); t++) {
+      for (let t = 0; t < Math.min(e.data.languages.length, 4); t++) {
         const n = e.data.languages[t];
         const { name: i, percent: s, text: o } = n;
         const a = [
